2537a7bf45a2e5e219d2f59a159f710981676702
Antonio Serafimovski <serafimovskiantonio3@gmail.com>
2022-11-12 20:59:25 +0100

    Update application.py

--- application.py
+++ application.py
@@ -1,11 +1,3 @@
-# ------------------------------------------------
-# Program by Denis Astahov
-#
-#
-# Version      Date           Info
-# 1.0          13-Dec-2019    Initial Version
-#
-# ----------------------------------------------
 from flask import Flask, render_template
 
 application = Flask(__name__)
@@ -15,16 +7,7 @@ application = Flask(__name__)
 def root():
     return render_template("index.html")
 
-@application.route("/help")
-def helppage():
-    return render_template("help.html")
 
-@application.route("/hello")
-def index():
-    return "Hello World from Flask Hello Page.<b> v1.0"
-
-#--------Main------------------
 if __name__ == "__main__":
     application.debug = True
     application.run()
-#------------------------------
